c3375ff15ae7f9a7932a2a0a9530cf6d1dda74b9
dangyours <48520458+dangyours@users.noreply.github.com>
2019-10-13 12:14:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ var config = {
         "心爱的小可爱",
         "今天是你的生日",
         "这是我们在一起的",
-        "第三个生日了哦",
+        "第二个生日了哦",
         "去年的生日",
         "还记得在王婆串串吃的呢",
         "今年要吃好的喽哦",
@@ -17,9 +17,9 @@ var config = {
     ],
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        turn_on: "点我",
+        play: "music~",
+        bannar_coming: "爱心",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
